db374cc057240147902f046dd5adf4509597d6ff
lawrence618 <jose_aquino@cody.inc>
2025-07-31 17:22:34 +0800

    Add movie project

---
+++ src/components/Search.tsx
@@ -0,0 +1,22 @@
+
+
+const Search = ({searchTerm, setSearchTerm}) => {
+
+  return (
+    <div className='search'>
+      <div>
+        <img src="searchIcon.svg" alt="search" />
+
+        <input
+          type="text"
+          placeholder='Search for a movie...'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        
+      </div>
+    </div>
+  )
+}
+
+export default Search
---
+++ src/components/TrendMovieList.tsx
@@ -0,0 +1,22 @@
+const TrendMovieList = ({trendingMovies}) => {
+  return (
+      <>
+        {trendingMovies.length > 0 && (
+          <section className="trending">
+            <h2>Trending Movies</h2>
+
+            <ul>
+              {trendingMovies.map((movie:any, index:any) => (
+                <li key={movie.$id}>
+                  <p>{index + 1}</p>
+                  <img src={movie.poster_url} alt={movie.title} />
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
+      </>
+  )
+}
+
+export default TrendMovieList
